343fc99bea507ba815d769ada178acc8060cdeb8
vivekrathod5 <vivekrathod536@gmail.com>
2022-05-12 16:16:29 +0530

    initial commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
-    # 'rest_framework.authtokan',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
     'user',
  
